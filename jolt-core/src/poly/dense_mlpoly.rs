978ebf0ef45ec51146ed5838a7d80d183b24dde5
Ari <software@randomwalks.xyz>
2025-10-15 06:11:42 +0100

    optimisation: some binds are sequential now to debug issues
    with_min_len seems to have sorted the benches/binding out

--- jolt-core/src/poly/dense_mlpoly.rs
+++ jolt-core/src/poly/dense_mlpoly.rs
@@ -89,11 +89,9 @@ impl<F: JoltField> DensePolynomial<F> {
         let n = self.len() / 2;
         let (left, right) = self.Z.split_at_mut(n);
 
-        left.par_iter_mut()
-            .zip(right.par_iter())
-            .for_each(|(a, b)| {
-                *a += *r * (*b - *a);
-            });
+        left.iter_mut().zip(right.iter()).for_each(|(a, b)| {
+            *a += *r * (*b - *a);
+        });
 
         self.num_vars -= 1;
         self.len = n;
@@ -125,26 +123,15 @@ impl<F: JoltField> DensePolynomial<F> {
     pub fn bound_poly_var_top_zero_optimized(&mut self, r: &F::Challenge) {
         let n = self.len() / 2;
 
-        if self.num_vars >= 18 {
-            let (left, right) = self.Z.split_at_mut(n);
+        let (left, right) = self.Z.split_at_mut(n);
 
-            left.par_iter_mut()
-                .zip(right.par_iter())
-                .filter(|(&mut a, &b)| a != b)
-                .for_each(|(a, b)| {
-                    *a += *r * (*b - *a);
-                });
-        } else {
-            let (left, right) = self.Z.split_at_mut(n);
-
-            left.iter_mut()
-                .zip(right.iter())
-                .filter(|(&mut a, &b)| a != b)
-                .for_each(|(a, b)| {
-                    *a += *r * (*b - *a);
-                });
-        }
-
+        left.par_iter_mut()
+            .zip(right.par_iter())
+            .with_min_len(4096)
+            .filter(|(&mut a, &b)| a != b)
+            .for_each(|(a, b)| {
+                *a += *r * (*b - *a);
+            });
         self.num_vars -= 1;
         self.len = n;
     }
