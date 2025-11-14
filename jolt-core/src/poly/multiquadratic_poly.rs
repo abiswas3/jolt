b15f3ede945708f4ef2596270ec63fb7444aee96
Quang Dao <qvd@andrew.cmu.edu>
2025-11-14 16:39:10 -0700

    add special cases

--- jolt-core/src/poly/multiquadratic_poly.rs
+++ jolt-core/src/poly/multiquadratic_poly.rs
@@ -57,6 +57,35 @@ impl<F: JoltField> MultiquadraticPolynomial<F> {
         tmp: &mut [F],    // scratch buffer, also (size 3^dim)
         dim: usize,
     ) {
+        let in_size = 1usize << dim;
+        let out_size = 3usize.pow(dim as u32);
+
+        assert_eq!(input.len(), in_size);
+        assert_eq!(output.len(), out_size);
+        assert_eq!(tmp.len(), out_size);
+
+        match dim {
+            0 => {
+                if !input.is_empty() {
+                    output[0] = input[0];
+                }
+                return;
+            }
+            1 => {
+                Self::expand_linear_dim1(input, output);
+                return;
+            }
+            2 => {
+                Self::expand_linear_dim2(input, output);
+                return;
+            }
+            3 => {
+                Self::expand_linear_dim3(input, output);
+                return;
+            }
+            _ => {}
+        }
+
         // Fill output by expanding one dimension at a time.
         // We treat slices of increasing "arity"
 
@@ -72,9 +101,7 @@ impl<F: JoltField> MultiquadraticPolynomial<F> {
         };
 
         let mut in_stride = 1usize;
-        let in_size = 1 << dim;
         let mut out_stride = 1usize;
-        let out_size = 3usize.pow(dim as u32);
         let mut blocks = 1 << (dim - 1);
 
         // sanity checks
@@ -103,6 +130,166 @@ impl<F: JoltField> MultiquadraticPolynomial<F> {
             out_stride *= 3;
             blocks /= 2;
         }
+    }
+
+    #[inline(always)]
+    fn expand_linear_dim1(input: &[F], output: &mut [F]) {
+        debug_assert_eq!(input.len(), 2);
+        debug_assert_eq!(output.len(), 3);
+
+        let f0 = input[0];
+        let f1 = input[1];
+
+        output[0] = f0;
+        output[1] = f1;
+        output[2] = f1 - f0;
+    }
+
+    #[inline(always)]
+    fn expand_linear_dim2(input: &[F], output: &mut [F]) {
+        debug_assert_eq!(input.len(), 4);
+        debug_assert_eq!(output.len(), 9);
+
+        let f00 = input[0]; // f(0,0)
+        let f01 = input[1]; // f(0,1)
+        let f10 = input[2]; // f(1,0)
+        let f11 = input[3]; // f(1,1)
+
+        // First extrapolate along the fastest-varying variable (second coordinate).
+        let a00 = f00;
+        let a01 = f01;
+        let a0_inf = f01 - f00;
+
+        let a10 = f10;
+        let a11 = f11;
+        let a1_inf = f11 - f10;
+
+        // Then extrapolate along the remaining variable.
+        let inf0 = a10 - a00;
+        let inf1 = a11 - a01;
+        let inf_inf = a1_inf - a0_inf;
+
+        // Layout: index = 3 * enc(x0) + enc(x1), x1 fastest, enc: {0,1,∞} -> {0,1,2}.
+        output[0] = a00;      // (0,0)
+        output[1] = a01;      // (0,1)
+        output[2] = a0_inf;   // (0,∞)
+
+        output[3] = a10;      // (1,0)
+        output[4] = a11;      // (1,1)
+        output[5] = a1_inf;   // (1,∞)
+
+        output[6] = inf0;     // (∞,0)
+        output[7] = inf1;     // (∞,1)
+        output[8] = inf_inf;  // (∞,∞)
+    }
+
+    #[inline(always)]
+    fn expand_linear_dim3(input: &[F], output: &mut [F]) {
+        debug_assert_eq!(input.len(), 8);
+        debug_assert_eq!(output.len(), 27);
+
+        // Corner values f(x0, x1, x2) with x2 fastest.
+        let f000 = input[0];
+        let f001 = input[1];
+        let f010 = input[2];
+        let f011 = input[3];
+        let f100 = input[4];
+        let f101 = input[5];
+        let f110 = input[6];
+        let f111 = input[7];
+
+        // Stage 1: extrapolate along x2 (fastest variable) for each (x0, x1).
+        let g000 = f000;
+        let g001 = f001;
+        let g00_inf = f001 - f000;
+
+        let g010 = f010;
+        let g011 = f011;
+        let g01_inf = f011 - f010;
+
+        let g100 = f100;
+        let g101 = f101;
+        let g10_inf = f101 - f100;
+
+        let g110 = f110;
+        let g111 = f111;
+        let g11_inf = f111 - f110;
+
+        // Stage 2: extrapolate along x1 for each (x0, x2).
+        // x0 = 0
+        let h0_0_0 = g000;
+        let h0_1_0 = g010;
+        let h0_inf_0 = g010 - g000;
+
+        let h0_0_1 = g001;
+        let h0_1_1 = g011;
+        let h0_inf_1 = g011 - g001;
+
+        let h0_0_inf = g00_inf;
+        let h0_1_inf = g01_inf;
+        let h0_inf_inf = g01_inf - g00_inf;
+
+        // x0 = 1
+        let h1_0_0 = g100;
+        let h1_1_0 = g110;
+        let h1_inf_0 = g110 - g100;
+
+        let h1_0_1 = g101;
+        let h1_1_1 = g111;
+        let h1_inf_1 = g111 - g101;
+
+        let h1_0_inf = g10_inf;
+        let h1_1_inf = g11_inf;
+        let h1_inf_inf = g11_inf - g10_inf;
+
+        // Stage 3: extrapolate along x0 for each (x1, x2).
+        // Index: idx(x0, x1, x2) = 9 * enc(x0) + 3 * enc(x1) + enc(x2),
+        // enc: {0,1,∞} -> {0,1,2}, x2 fastest.
+
+        // (x1, x2) = (0, 0)
+        output[0] = h0_0_0;                     // (0,0,0)
+        output[9] = h1_0_0;                     // (1,0,0)
+        output[18] = h1_0_0 - h0_0_0;           // (∞,0,0)
+
+        // (0, 1)
+        output[1] = h0_0_1;                     // (0,0,1)
+        output[10] = h1_0_1;                    // (1,0,1)
+        output[19] = h1_0_1 - h0_0_1;           // (∞,0,1)
+
+        // (0, ∞)
+        output[2] = h0_0_inf;                   // (0,0,∞)
+        output[11] = h1_0_inf;                  // (1,0,∞)
+        output[20] = h1_0_inf - h0_0_inf;       // (∞,0,∞)
+
+        // (1, 0)
+        output[3] = h0_1_0;                     // (0,1,0)
+        output[12] = h1_1_0;                    // (1,1,0)
+        output[21] = h1_1_0 - h0_1_0;           // (∞,1,0)
+
+        // (1, 1)
+        output[4] = h0_1_1;                     // (0,1,1)
+        output[13] = h1_1_1;                    // (1,1,1)
+        output[22] = h1_1_1 - h0_1_1;           // (∞,1,1)
+
+        // (1, ∞)
+        output[5] = h0_1_inf;                   // (0,1,∞)
+        output[14] = h1_1_inf;                  // (1,1,∞)
+        output[23] = h1_1_inf - h0_1_inf;       // (∞,1,∞)
+
+        // (∞, 0)
+        output[6] = h0_inf_0;                   // (0,∞,0)
+        output[15] = h1_inf_0;                  // (1,∞,0)
+        output[24] = h1_inf_0 - h0_inf_0;       // (∞,∞,0)
+
+        // (∞, 1)
+        output[7] = h0_inf_1;                   // (0,∞,1)
+        output[16] = h1_inf_1;                  // (1,∞,1)
+        output[25] = h1_inf_1 - h0_inf_1;       // (∞,∞,1)
+
+        // (∞, ∞)
+        output[8] = h0_inf_inf;                 // (0,∞,∞)
+        output[17] = h1_inf_inf;                // (1,∞,∞)
+        output[26] = h1_inf_inf - h0_inf_inf;   // (∞,∞,∞)
     }
 
     /// Bind the first (least-significant) variable z_0 := r, reducing the
